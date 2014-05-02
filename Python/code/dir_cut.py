70e533f149bb9fa9b8abf5735646cd5040ab7680
Antoine <manto235@gmail.com>
2014-05-02 16:39:39 +0200

    Réduction code exemple

---
+++ Python/code/dir_cut.py
@@ -0,0 +1,6 @@
+>>> type(42)
+<type 'int'>
+>>> isinstance(42, str)
+False
+>>> dir(42)
+['__abs__', '__add__', '__and__', '__class__', '__cmp__', ..., '__truediv__', '__trunc__', '__xor__', 'bit_length', 'conjugate', 'denominator', 'imag', 'numerator', 'real']
