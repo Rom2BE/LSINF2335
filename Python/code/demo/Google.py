1658b544d7c1687111e4ffc9b07e662dc86eacd0
Rom2BE <capron.romain@gmail.com>
2014-05-02 16:25:58 +0200

    Killer-app + quine in 2.2

---
+++ Python/code/demo/Google.py
@@ -0,0 +1,11 @@
+class Person(message.Message):
+    __metaclass__ = reflection.GeneratedProtocolMessageType
+    
+class PhoneNumber(message.Message):
+    __metaclass__ = reflection.GeneratedProtocolMessageType
+    DESCRIPTOR = _PERSON_PHONENUMBER
+    DESCRIPTOR = _PERSON
+
+class AddressBook(message.Message):
+    __metaclass__ = reflection.GeneratedProtocolMessageType
+    DESCRIPTOR = _ADDRESSBOOK
